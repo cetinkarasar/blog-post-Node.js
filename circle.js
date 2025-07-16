a6e8d998c32f23238f378b0875a540e9c1f7b7ad
cetinkarasar <cetinkarasar257@gmail.com>
2025-07-16 11:24:33 +0300

    İlk commit: blog ekleme ve listeleme

---
+++ circle.js
@@ -0,0 +1,12 @@
+function circleArea(r){
+    console.log(Math.PI *r*r);
+}
+
+function circleCircumference(r){
+    console.log( 2* Math.PI *r);
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+};
